3deeb0ab76f00b13d6ed5adb5827d51de567d83e
Mirac Can Yilmaz <miraccan6161@gmail.com>
2025-01-18 13:38:21 +0300

    feature-7

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ def new_feature():
     print("New feature added")
 def some_feature_added():
     print("Some feature added")
+    
+def some_feature_added_2():
+    print("Some feature added")
+    
+def some_feature_added_3():
+    print("Some feature added")
